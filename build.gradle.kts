6718ae6fad51de765fc811c7c01100897cf83091
Lou Böcker <lou@boecker.dev>
2026-06-22 22:40:43 +0200

    update permissions-api for reload ranks command

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     implementation("net.minestom:minestom:2026.06.20-26.1.2")
     implementation("dev.hollowcube:polar:1.16.0")
 
-    implementation("dev.boecker.cherrycave.permissions:minestom:0.1.2")
+    implementation("dev.boecker.cherrycave.permissions:minestom:0.1.3")
 
     implementation("ch.qos.logback:logback-classic:1.5.34")
     implementation("io.github.oshai:kotlin-logging-jvm:8.0.4")
